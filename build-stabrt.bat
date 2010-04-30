0d290002cce09e557e0810bec5ebc9271ff0b0f6
stab.hacker <stab.hacker@ce3443ec-90ab-a844-b226-27f7e8e8bd6a>
2010-04-30 02:22:13 +0000

    Bootstrapped the compilation of lambda expressions to expression trees.

--- build-stabrt.bat
+++ build-stabrt.bat
@@ -2,6 +2,6 @@
 
 ECHO Compiling source code...
 
-java -jar bin\stabc.jar -al:bin\stabal.jar -doc:runtime\doc.xml -out:runtime\stabrt.jar runtime\sources\lang\*.stab runtime\sources\query\*.stab
+java -jar bin\stabc.jar -al:bin\stabal.jar -doc:runtime\doc.xml -out:runtime\stabrt.jar runtime\sources\lang\*.stab runtime\sources\query\*.stab runtime\sources\tree\*.stab
 
 ECHO Done.
