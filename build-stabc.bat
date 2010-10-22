626e2657d357713240da5f9ea1df508bb179cdf7
stab.hacker <stab.hacker@ce3443ec-90ab-a844-b226-27f7e8e8bd6a>
2010-10-22 15:00:00 +0000

    Upgraded ASM to version 3.3 Minor changes

--- build-stabc.bat
+++ build-stabc.bat
@@ -2,6 +2,6 @@
 
 ECHO Compiling source code...
 
-java -jar bin\stabc.jar -al:bin\stabal.jar -cp:bin\stabrt.jar;bin\asm-3.2.jar -resources:compiler\resources -manifest:compiler\MANIFEST.MF -out:compiler\stabc.jar compiler\sources\parser\*.stab compiler\sources\helpers\*.stab compiler\sources\syntaxtree\*.stab compiler\sources\bytecode\*.stab compiler\sources\bytecode\signature\*.stab compiler\sources\compiler\*.stab
+java -jar bin\stabc.jar -al:bin\stabal.jar -cp:bin\stabrt.jar;bin\asm-3.3.jar -resources:compiler\resources -manifest:compiler\MANIFEST.MF -out:compiler\stabc.jar compiler\sources\parser\*.stab compiler\sources\helpers\*.stab compiler\sources\syntaxtree\*.stab compiler\sources\bytecode\*.stab compiler\sources\bytecode\signature\*.stab compiler\sources\compiler\*.stab
 
 ECHO Done.
